6af60fad4157c7a286a1f31fa44c8b30e409c9c0
Htan <66896208+DryPringleSoup@users.noreply.github.com>
2024-08-31 21:18:56 -0400

    Update CTT.ps1

--- src/CTT.ps1
+++ src/CTT.ps1
@@ -6,10 +6,10 @@ function Run-CommandElevated {
     
     $psi = New-Object System.Diagnostics.ProcessStartInfo
     $psi.FileName = "powershell.exe"
-    $psi.Arguments = "-NoProfile -ExecutionPolicy Bypass -Command `"$c`""
+    $psi.Arguments = "-NoProfile -ExecutionPolicy Bypass -Command `"$Command`""
     $psi.Verb = "RunAs"
     
     [System.Diagnostics.Process]::Start($psi) | Out-Null
 }
 
-Run-CommandElevated -c "iwr -useb https://christitus.com/win | iex"
+Run-CommandElevated -Command "iwr -useb https://christitus.com/win | iex"
