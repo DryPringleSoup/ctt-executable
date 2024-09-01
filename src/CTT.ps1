7399c56045cbe38d7d872245a08593143fdac37e
Htan <66896208+DryPringleSoup@users.noreply.github.com>
2024-08-31 21:15:43 -0400

    Update CTT.ps1

--- src/CTT.ps1
+++ src/CTT.ps1
@@ -1,22 +1,15 @@
-# Function to run a command in elevated mode
 function Run-CommandElevated {
     param(
         [Parameter(Mandatory=$true)]
         [string]$Command
     )
     
-    # Create a new ProcessStartInfo object
     $psi = New-Object System.Diagnostics.ProcessStartInfo
     $psi.FileName = "powershell.exe"
-    $psi.Arguments = "-NoProfile -ExecutionPolicy Bypass -Command `"$Command`""
+    $psi.Arguments = "-NoProfile -ExecutionPolicy Bypass -Command `"$c`""
     $psi.Verb = "RunAs"
     
-    # Start the process
     [System.Diagnostics.Process]::Start($psi) | Out-Null
 }
 
-# Define the command to execute
-$commandToExecute = "iwr -useb https://christitus.com/win | iex"
-
-# Run the command in elevated mode
-Run-CommandElevated -Command $commandToExecute
+Run-CommandElevated -c "iwr -useb https://christitus.com/win | iex"
