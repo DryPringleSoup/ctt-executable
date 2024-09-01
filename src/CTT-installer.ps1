f2203719573fcf2dbd4205de012fcf4e622447b4
Htan <66896208+DryPringleSoup@users.noreply.github.com>
2024-08-31 21:16:25 -0400

    Update CTT-installer.ps1

--- src/CTT-installer.ps1
+++ src/CTT-installer.ps1
@@ -1,11 +1,10 @@
 $repoUrl = "https://github.com/DryPringleSoup/ctt-executable/raw/main/"
 $filePath = "CTT.exe"
-$gitUrl = $repoUrl + $filePath #make raw file url
-
-$desktopPath = [Environment]::GetFolderPath("Desktop") #get desktop folder path
-$outputPath = Join-Path -Path $desktopPath -ChildPath $filePath #desktop file output path
+$gitUrl = $repoUrl + $filePath
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+$outputPath = Join-Path -Path $desktopPath -ChildPath $filePath
 
 try {
-    Invoke-WebRequest -Uri $gitUrl -OutFile $outputPath #download the file
-    Write-Host "File downloaded successfully: $outputPath" #report successful download
+    Invoke-WebRequest -Uri $gitUrl -OutFile $outputPath
+    Write-Host "File downloaded successfully: $outputPath"
 } catch {Write-Host "Error occurred: $_"}
